84cf82d9ab110b6bc63ba4c314e3b155f03a2997
riched <rickard.hedlund@lm.se>
2025-02-24 08:51:04 +0100

    add knqyf263/pet

---
+++ srcpkgs/pet/update-pet.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+printf "Checking latest version\n"
+
+__dir="$(dirname "${BASH_SOURCE[0]}")"
+
+LATEST_VERSION=$(gh release list --repo knqyf263/pet --json name,isLatest --jq '.[] | select(.isLatest)|.name')
+export VERSION=${LATEST_VERSION#"v"}
+CURRENT_VERSION=$(grep -E '^version=' ${__dir}/template | cut -d= -f2)
+
+printf "Latest version is: %s\nLatest built version is: %s\n" "${VERSION}" "${CURRENT_VERSION}"
+[ "${CURRENT_VERSION}" = "${VERSION}" ] && printf "No new version to release\n" && exit 0
+
+# No preprepped checksum files, need to download the binary and calculate it myself
+gh release download -R knqyf263/pet --archive=tar.gz --output "pet.tar.gz"
+export SHA256=$(sha256sum ./pet.tar.xz | cut -d ' ' -f1 )
+rm ./pet.tar.xz
+[[ ! ${SHA256} =~ ^[a-z0-9]+$ ]] && printf "got junk instead of sha256\n" && exit 1
+
+envsubst '${SHA256} ${VERSION}' < ${__dir}/.template > ${__dir}/template
+
+printf "pet template updated\n"

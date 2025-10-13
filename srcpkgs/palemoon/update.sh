9840db4bd40c941e91ef72512df9600efd20729e
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-10-13 16:33:00 +0300

    Update update.sh

--- srcpkgs/palemoon/update.sh
+++ srcpkgs/palemoon/update.sh
@@ -4,14 +4,14 @@ printf "Checking latest version\n"
 
 __dir="$(dirname "${BASH_SOURCE[0]}")"
 
-LATEST_VERSION=$(gh issue list --repo RealityRipple/Pale-Moon --exclude-drafts --exclude-pre-releases --json name,tagName,isLatest --jq '.[] | select(.isLatest)|.tagName')
+LATEST_VERSION=$(gh release list --repo RealityRipple/Pale-Moon --exclude-drafts --exclude-pre-releases --json name,tagName,isLatest --jq '.[] | select(.isLatest)|.tagName')
 export VERSION=${LATEST_VERSION#"v"}
 CURRENT_VERSION=$(grep -E '^version=' ${__dir}/template | cut -d= -f2)
 
 printf "Latest version is: %s\nLatest built version is: %s\n" "${VERSION}" "${CURRENT_VERSION}"
 [ "${CURRENT_VERSION}" = "${VERSION}" ] && printf "No new version to release\n" && exit 0
 
-export SHA256=$(curl -L https://repo.palemoon.org/MoonchildProductions/Pale-Moon/archive/${VERSION}/archive/refs/tags/${VERSION}.tar.gz.sha256 | cut -d ' ' -f1 )
+export SHA256=$(curl -L https://github.com/RealityRipple/Pale-Moon/archive/refs/tags/${VERSION}/archive/refs/tags/${VERSION}.tar.gz.sha256 | cut -d ' ' -f1 )
 [[ ! ${SHA256} =~ ^[a-z0-9]+$ ]] && printf "got junk instead of sha256\n" && exit 1
 
 envsubst '${SHA256} ${VERSION}' < ${__dir}/.template > ${__dir}/template

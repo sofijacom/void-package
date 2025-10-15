b876710f359455de7b4788202d7c962584163036
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-10-15 16:00:11 +0300

    Update update.sh

--- srcpkgs/yandex-browser/update.sh
+++ srcpkgs/yandex-browser/update.sh
@@ -7,14 +7,14 @@ printf "Checking latest version\n"
 
 __dir="$(dirname "${BASH_SOURCE[0]}")"
 
-LATEST_VERSION=$(curl -Ss --request GET "https://repo.yandex.ru/yandex-browser/deb/pool/main/y/*/*deb" | jq -r '.[0] | .name')
+LATEST_VERSION=$(curl -Ss --request GET "https://repo.yandex.ru/yandex-browser/deb/pool/main/y/yandex-browser-stable/*deb" | jq -r '.[0] | .name')
 export VERSION=${LATEST_VERSION#"v"}
 CURRENT_VERSION=$(grep -E '^version=' ${__dir}/template | cut -d= -f2)
 
 printf "Latest version is: %s\nLatest built version is: %s\n" "${VERSION}" "${CURRENT_VERSION}"
 [ "${CURRENT_VERSION}" = "${VERSION}" ] && printf "No new version to release\n" && exit 0
 
-export SHA256=$(curl -L https://repo.yandex.ru/yandex-browser/deb/pool/main/y/yandex-browser-stable/${LATEST_VERSION}/yandex-browser-stable_${VERSION}_amd64.deb.sha256 | cut -d ' ' -f1 )
+export SHA256=$(curl -L https://repo.yandex.ru/yandex-browser/deb/pool/main/y/${LATEST_VERSION}/yandex-browser-stable_${VERSION}_amd64.deb.sha256 | cut -d ' ' -f1 )
 [[ ! ${SHA256} =~ ^[a-z0-9]+$ ]] && printf "got junk instead of sha256\n" && exit 1
 
 envsubst '${SHA256} ${VERSION}' < ${__dir}/.template > ${__dir}/template

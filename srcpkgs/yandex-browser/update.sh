109665d928c4244b270b7390537c3e44f7744281
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-10-17 00:04:07 +0300

    Remove commented version from update script
    Signed-off-by: Sofiya <107557749+sofijacom@users.noreply.github.com>

--- srcpkgs/yandex-browser/update.sh
+++ srcpkgs/yandex-browser/update.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-# yandex-browser-stable_25.8.1.890-1_amd64.deb
 site="https://repo.yandex.ru/yandex-browser/deb/pool/main/y/yandex-browser-stable/"
 pattern="yandex-browser-stable_\K\d+\.\d+\.\d+\.\d+(?=_amd64\.deb)"

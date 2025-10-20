ee069e52eb5413ba289547cf9485d74c2386242a
Sofiya <107557749+sofijacom@users.noreply.github.com>
2025-10-20 17:47:31 +0300

    Delete srcpkgs/ungoogled-chromium directory
    Signed-off-by: Sofiya <107557749+sofijacom@users.noreply.github.com>

--- srcpkgs/ungoogled-chromium/files/chromium.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export CHROME_WRAPPER=/usr/lib/ungoogled-chromium/ungoogled-chromium
-export CHROME_DESKTOP=ungoogled-chromium.desktop
-CHROME_FLAGS="--enable-gpu-rasterization $CHROME_FLAGS"
-exec /usr/lib/ungoogled-chromium/ungoogled-chromium $CHROME_FLAGS "$@"

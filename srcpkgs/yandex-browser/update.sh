9b44b64593ff60195d083adddf85e680fc92ee4c
Sofiya <sofija.p2018@gmail.com>
2025-11-01 20:31:43 +0300

    Update update.sh

--- srcpkgs/yandex-browser/update.sh
+++ srcpkgs/yandex-browser/update.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-site="https://repo.yandex.ru/yandex-browser/deb/pool/main/y/yandex-browser-stable/"
-pattern="yandex-browser-stable_\K\d+\.\d+\.\d+\.\d+(?=_amd64\.deb)"
+#site="https://repo.yandex.ru/yandex-browser/deb/pool/main/y/yandex-browser-stable/"
+#pattern="yandex-browser-stable_\K\d+\.\d+\.\d+\.\d+(?=_amd64\.deb)"
 
-#printf "Checking latest version\n"
+printf "Checking latest version\n"
 
-#__dir="$(dirname "${BASH_SOURCE[0]}")"
+__dir="$(dirname "${BASH_SOURCE[0]}")"
 
 #LATEST_VERSION=$(curl -Ss --request GET "https://repo.yandex.ru/yandex-browser/deb/pool/main/y/yandex-browser-stable/" | jq -r '.[0] | .name')
-#LATEST_VERSION=$(curl -Ss --request GET "https://repo.yandex.ru/yandex-browser/deb/pool/main/y/yandex-browser-stable/" | jq -r '.[0] | .name')
-#export VERSION=${LATEST_VERSION#"v"}
-#CURRENT_VERSION=$(grep -E '^version=' ${__dir}/template | cut -d= -f2)
+LATEST_VERSION=$(curl -Ls -o /dev/null -w '%{url_effective}' https://repo.yandex.ru/yandex-browser/deb/pool/main/y/yandex-browser-stable/)
+export VERSION=${LATEST_VERSION#"v"}
+CURRENT_VERSION=$(grep -E '^version=' ${__dir}/template | cut -d= -f2)
 
-#printf "Latest version is: %s\nLatest built version is: %s\n" "${VERSION}" "${CURRENT_VERSION}"
-#[ "${CURRENT_VERSION}" = "${VERSION}" ] && printf "No new version to release\n" && exit 0
+printf "Latest version is: %s\nLatest built version is: %s\n" "${VERSION}" "${CURRENT_VERSION}"
+[ "${CURRENT_VERSION}" = "${VERSION}" ] && printf "No new version to release\n" && exit 0
 
-#export SHA256=$(curl -L https://repo.yandex.ru/yandex-browser/deb/pool/main/y/yandex-browser-stable/v${VERSION}/yandex-browser-stable_${VERSION}_amd64.deb.sha256 | cut -d ' ' -f1 )
-#[[ ! ${SHA256} =~ ^[a-z0-9]+$ ]] && printf "got junk instead of sha256\n" && exit 1
+export SHA256=$(curl -L https://repo.yandex.ru/yandex-browser/deb/pool/main/y/yandex-browser-stable/v${VERSION}/yandex-browser-stable_${VERSION}_amd64.deb.sha256 | cut -d ' ' -f1 )
+[[ ! ${SHA256} =~ ^[a-z0-9]+$ ]] && printf "got junk instead of sha256\n" && exit 1
 
-#envsubst '${SHA256} ${VERSION}' < ${__dir}/.template > ${__dir}/template
+envsubst '${SHA256} ${VERSION}' < ${__dir}/.template > ${__dir}/template
 
-#printf "Yandex template updated\n"
+printf "Yandex template updated\n"

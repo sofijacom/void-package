81a145b9b1f1c4501c9df1bc2498883807901071
sofijacom <sofija.p2018@gmail.com>
2026-04-26 13:49:54 +0300

    Update

--- srcpkgs/microsoft-edge/update.sh
+++ srcpkgs/microsoft-edge/update.sh
@@ -2,29 +2,9 @@
 
 printf "Checking latest version\n"
 
-__dir="$(dirname "${BASH_SOURCE[0]}")"
-
-LATEST_VERSION=$(git ls-remote --tags --refs https://github.com/NDViet/microsoft-edge-stable | grep -o 'microsoft-edge-stable-\(v\)\?[0-9]\+\.[0-9]\+\.[0-9]\+-[0-9]\{8,14\}$' \
-  | awk -F- '
-      {
-        ts = $NF
-        while (length(ts) < 14) ts = ts "0"
-        print ts, $0
-      }
-    ' \
-  | sort -nr | head -n1 | cut -d' ' -f2-
-)
-VERSION=${LATEST_VERSION#"v"}
-CUR_VERSION=$(grep -E '^version=' ${__dir}/template | cut -d= -f2)
-CUR_TIMESTAMP=$(grep -E '^timestamp=' ${__dir}/template | cut -d= -f2)
-CURRENT_VERSION=$(printf "%s-%s" "${CUR_VERSION}" "${CUR_TIMESTAMP}")
-
-printf "Latest version is: %s\nLatest built version is: %s\n" "${VERSION}" "${CURRENT_VERSION}"
-[ "${CURRENT_VERSION}" = "${VERSION}" ] && printf "No new version to release\n" && exit 0
-
-export TIMESTAMP=${VERSION##*-}
-export VERSION=${VERSION%-*}
-
+LATEST_VERSION=$(gh release list --repo NDViet/microsoft-edge --json name,tagName,isLatest --jq '.[] | select(.isLatest)|.tagName')
+export VERSION=${LATEST_VERSION#"v"}
+CURRENT_VERSION=$(grep -E '^version=' ${__dir}/template | cut -d= -f2)
 
 printf "Latest version is: %s\nLatest built version is: %s\n" "${VERSION}" "${CURRENT_VERSION}"
 [ "${CURRENT_VERSION}" = "${VERSION}" ] && printf "No new version to release\n" && exit 0
@@ -33,7 +13,7 @@ export SHA256=$(curl -sL https://github.com/NDViet/microsoft-edge-stable/release
 ## | cut -d ' ' -f1 )
 [[ ! ${SHA256} =~ ^[a-z0-9]+$ ]] && printf "got junk instead of sha256\n" && exit 1
 
-envsubst '${SHA256} ${VERSION} ${TIMESTAMP}' < ${__dir}/.template > ${__dir}/template
+envsubst '${SHA256} ${VERSION}' < ${__dir}/.template > ${__dir}/template
 
 printf "microsoft-edge-stable template updated\n"
 

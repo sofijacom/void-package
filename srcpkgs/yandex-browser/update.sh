264b82cfabce0d7b73e90f10e816ab33d41ea64f
sofijacom <sofija.p2018@gmail.com>
2026-06-01 20:46:13 +0300

    Update

--- srcpkgs/yandex-browser/update.sh
+++ srcpkgs/yandex-browser/update.sh
@@ -6,8 +6,8 @@
 
 set -e
 
-APP="yandex-browser"
 TPL="srcpkgs/yandex-browser/template"
+APP="yandex-browser"
 CHANNEL="stable"
 URL="https://repo.yandex.ru/yandex-browser/deb/pool/main/y/yandex-browser-stable/"
 
@@ -15,25 +15,24 @@ echo "### Checking for yandex-browser updates..."
 
 CURRENT_VERSION=$(grep '^version=' "$TPL" | cut -d= -f2)
 
-# Ambil versi terbaru dari Chrome version API
-LATEST_VERSION=$(curl -Ls "https://repo.yandex.ru/yandex-browser/deb/pool/main/y/$APP-$CHANNEL/" | tr '">< ' '\n' | grep ".*amd64.deb" | tail -1)
-VERSION=$(cat control | grep Version | cut -c 10-)
-#    | grep -oP '"version":"\K[^"]+' | head -1)
+# Ambil versi terbaru dari Yandex version API
+LATEST_VERSION=$(curl -Ls "https://repo.yandex.ru/yandex-browser/deb/pool/main/y/$APP-$CHANNEL/" | tr '">< ' '\n' | grep ".*amd64.deb" | tail -1)" \
+    | grep -oP '"version":"\K[^"]+' | head -1)
 
 
-if [ -z "$VERSION" ]; then
+if [ -z "$LATEST_VERSION" ]; then
     echo "Error: Failed to fetch latest version."
     exit 1
 fi
 
-if [ "$VERSION" = "$CURRENT_VERSION" ]; then
+if [ "$LATEST_VERSION" = "$CURRENT_VERSION" ]; then
     echo "No update required. Current version: $CURRENT_VERSION"
     exit 0
 fi
 
-echo "Update found: $CURRENT_VERSION -> $VERSION"
+echo "Update found: $CURRENT_VERSION -> $LATEST_VERSION"
 
-DEB_URL="${URL}yandex-browser-${CHANNEL}_${VERSION}-1_amd64.deb"
+DEB_URL="${URL}yandex-browser-${CHANNEL}_${LATEST_VERSION}-1_amd64.deb"
 
 echo "Calculating checksum..."
 CHK=$(curl -L -s "$DEB_URL" | sha256sum | awk '{print $1}')
@@ -45,9 +44,9 @@ fi
 
 echo "Checksum: $CHK"
 
-sed -i "s/^version=.*/version=$VERSION/" "$TPL"
+sed -i "s/^version=.*/version=$LATEST_VERSION/" "$TPL"
 sed -i "s/^revision=.*/revision=1/" "$TPL"
 sed -i "s/^checksum=.*/checksum=$CHK/" "$TPL"
 
-echo "NEW_VERSION=$VERSION" >> $GITHUB_ENV
-echo "### Done! yandex-browser updated to $VERSION"
+echo "NEW_VERSION=$LATEST_VERSION" >> $GITHUB_ENV
+echo "### Done! yandex-browser updated to $LATEST_VERSION"

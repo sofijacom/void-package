72dd0d444c93e8e25fd02e0a7c21dd8d85b3a485
sofijacom <sofija.p2018@gmail.com>
2026-06-02 00:03:15 +0300

    Update

--- srcpkgs/yandex-browser/update.sh
+++ srcpkgs/yandex-browser/update.sh
@@ -15,30 +15,13 @@ echo "### Checking for yandex-browser updates..."
 
 CURRENT_VERSION=$(grep '^version=' "$TPL" | cut -d= -f2)
 
-# Ambil versi terbaru dari Yandex version API
+# Yandex version
+LATEST_VERSION=$(curl -Ss --request GET "https://repo.yandex.ru/yandex-browser/deb/pool/main/y/$APP-$CHANNEL/" | jq -r '.[0] | .name')
+
 # LATEST_VERSION=$(curl -Ls "https://repo.yandex.ru/yandex-browser/deb/pool/main/y/$APP-$CHANNEL/" | tr '">< ' '\n' | grep ".*amd64.deb" | tail -1)
 
 # LATEST_VERSION=$(curl -Ls "https://" \
 #    | grep -oP '"version":"\K[^"]+' | head -1)
-
-_create_yandex_appimage(){
-	DEB=$(curl -Ls "https://repo.yandex.ru/yandex-browser/deb/pool/main/y/$APP-$CHANNEL/" | tr '">< ' '\n' | grep ".*amd64.deb" | tail -1)
-	wait
-	if wget --version | head -1 | grep -q ' 1.'; then
-		wget -q --no-verbose --show-progress --progress=bar "https://repo.yandex.ru/yandex-browser/deb/pool/main/y/$APP-$CHANNEL/$DEB" || exit 1
-	else
-		wget "https://repo.yandex.ru/yandex-browser/deb/pool/main/y/$APP-$CHANNEL/$DEB" || exit 1
-	fi
-	ar x ./*.deb
-	tar xf ./data.tar.xz
-	mkdir "$APP".AppDir
-	mv ./opt/*/*/* ./"$APP".AppDir/
-	mv ./usr/share/applications/yandex*.desktop ./"$APP".AppDir/
-	ICONNAME=$(cat ./"$APP".AppDir/*desktop | grep "Icon=" | head -1 | cut -c 6-)
-	cp ./"$APP".AppDir/*512.png ./"$APP".AppDir/"$ICONNAME".png || cp ./"$APP".AppDir/*256.png ./"$APP".AppDir/"$ICONNAME".png || cp ./"$APP".AppDir/*128.png ./"$APP".AppDir/"$ICONNAME".png
-	tar xf ./control.tar.xz
-	VERSION=$(cat control | grep Version | cut -c 10-)
-}
 
 
 if [ -z "$VERSION" ]; then

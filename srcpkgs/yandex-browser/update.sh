84055cc8edf1599d6f0453aa3ee757cb140071bf
sofijacom <sofija.p2018@gmail.com>
2026-06-01 21:40:19 +0300

    Update

--- srcpkgs/yandex-browser/update.sh
+++ srcpkgs/yandex-browser/update.sh
@@ -16,31 +16,23 @@ echo "### Checking for yandex-browser updates..."
 CURRENT_VERSION=$(grep '^version=' "$TPL" | cut -d= -f2)
 
 # Ambil versi terbaru dari Yandex version API
-LATEST_VERSION=$(curl -Ls "https://repo.yandex.ru/yandex-browser/deb/pool/main/y/$APP-$CHANNEL/" | tr '">< ' '\n' | grep ".*amd64.deb" | tail -1)
-wait
-if wget --version | head -1 | grep -q ' 1.'; then
-    wget -q --no-verbose --show-progress --progress=bar "https://repo.yandex.ru/yandex-browser/deb/pool/main/y/$APP-$CHANNEL/$DEB" || exit 1
-else
-    wget "https://repo.yandex.ru/yandex-browser/deb/pool/main/y/$APP-$CHANNEL/$DEB" || exit 1
-fi
-ar x ./*.deb
-tar xf ./control.tar.xz
-VERSION=$(cat control | grep Version | cut -c 10-)
+LATEST_VERSION=$(curl -Ls "https://" \
+    | grep -oP '"version":"\K[^"]+' | head -1)
 
 
-if [ -z "$VERSION" ]; then
+if [ -z "$LATEST_VERSION" ]; then
     echo "Error: Failed to fetch latest version."
     exit 1
 fi
 
-if [ "$VERSION" = "$CURRENT_VERSION" ]; then
+if [ "$LATEST_VERSION" = "$CURRENT_VERSION" ]; then
     echo "No update required. Current version: $CURRENT_VERSION"
     exit 0
 fi
 
-echo "Update found: $CURRENT_VERSION -> $VERSION"
+echo "Update found: $CURRENT_VERSION -> $LATEST_VERSION"
 
-DEB_URL="${URL}yandex-browser-${CHANNEL}_${VERSION}-1_amd64.deb"
+DEB_URL="${URL}yandex-browser-${CHANNEL}_${LATEST_VERSION}-1_amd64.deb"
 
 echo "Calculating checksum..."
 CHK=$(curl -L -s "$DEB_URL" | sha256sum | awk '{print $1}')
@@ -52,9 +44,9 @@ fi
 
 echo "Checksum: $CHK"
 
-sed -i "s/^version=.*/version=$VERSION/" "$TPL"
+sed -i "s/^version=.*/version=$LATEST_VERSION/" "$TPL"
 sed -i "s/^revision=.*/revision=1/" "$TPL"
 sed -i "s/^checksum=.*/checksum=$CHK/" "$TPL"
 
-echo "NEW_VERSION=$VERSION" >> $GITHUB_ENV
-echo "### Done! yandex-browser updated to $VERSION"
+echo "NEW_VERSION=$LATEST_VERSION" >> $GITHUB_ENV
+echo "### Done! yandex-browser updated to $LATEST_VERSION"

52b76152087435611e1731d1f04e3d02fafa230d
sofijacom <sofija.p2018@gmail.com>
2026-06-14 19:41:32 +0300

    Update

---
+++ srcpkgs/librewolf/update.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -e
+
+REPO="librewolf/source"
+TPL="srcpkgs/librewolf/template"
+
+__dir="$(dirname "${BASH_SOURCE[0]}")"
+
+echo "### Checking for librewolf updates..."
+
+# LATEST_VERSION=$(gh release list --repo ${REPO} --json name,tagName,isLatest --jq '.[] | select(.isLatest)|.tagName')
+LATEST_VERSION=$(gh api repos/$REPO/releases/latest --jq .tag_name | sed 's/^v//')
+
+VERSION=${LATEST_VERSION#"v"}
+CUR_VERSION=$(grep -E '^version=' ${__dir}/template | cut -d= -f2)
+CUR_TIMESTAMP=$(grep -E '^timestamp=' ${__dir}/template | cut -d= -f2)
+CURRENT_VERSION=$(printf "%s-%s" "${CUR_VERSION}" "${CUR_TIMESTAMP}")
+
+printf "Latest version is: %s\nLatest built version is: %s\n" "${VERSION}" "${CURRENT_VERSION}"
+[ "${CURRENT_VERSION}" = "${VERSION}" ] && printf "No new version to release\n" && exit 0
+
+export TIMESTAMP=${VERSION##*-}
+export VERSION=${VERSION%-*}
+
+if [ -z "$VERSION" ]; then
+    echo "Error: Failed to fetch latest version."
+    exit 1
+fi
+
+if [ "$VERSION" = "$CURRENT_VERSION" ]; then
+    echo "No update required. Current version: $CURRENT_VERSION"
+    exit 0
+fi
+
+echo "Update found: $CURRENT_VERSION -> $_VERSION"
+
+# URL_X86="https://github.com/$REPO/releases/download/${LATEST_VERSION}/zen.linux-x86_64.tar.xz"
+URL_X86="https://codeberg.org/api/packages/librewolf/generic/librewolf-source/${LATEST_VERSION}/librewolf-${LATEST_VERSION}.source.tar.gz
+
+echo "Calculating checksum..."
+CHK_X86=$(curl -L -s "$URL_X86" | sha256sum | awk '{print $1}')
+
+if [ -z "$CHK_X86" ]; then
+    echo "Error: Failed to fetch checksum."
+    exit 1
+fi
+
+echo "Checksum: $CHK_X86"
+
+sed -i "s/^version=.*/version=$VERSION/" "$TPL"
+sed -i "s/^timestamp=.*/timestamp=$TIMESTAMP/" "$TPL"
+sed -i "s/^checksum=.*/checksum=$CHK/" "$TPL"
+
+echo "NEW_VERSION=$VERSION" >> $GITHUB_ENV
+echo "### Done! librewolf updated to $VERSION"

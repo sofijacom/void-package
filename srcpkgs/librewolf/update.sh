fb9d2a3b601c2bd4932f5d518389c87cfc0a94af
sofijacom <sofija.p2018@gmail.com>
2026-06-14 22:08:00 +0300

    Update

--- srcpkgs/librewolf/update.sh
+++ srcpkgs/librewolf/update.sh
@@ -10,7 +10,7 @@ echo "### Checking for librewolf updates..."
 # https://codeberg.org/api/packages/librewolf/generic/librewolf-source/151.0.4-1/librewolf-151.0.4-1.source.tar.gz
 
 #PROJECT_ID="abf57e09399b44fba07424d3f56d76d807ddec4f"
-LATEST_VERSION=$(curl -Ss --request GET "https://codeberg.org/api/packages/librewolf/generic/librewolf-source/" | jq -r '.[0] | .name')
+LATEST_VERSION=$(curl -Ss --request GET "https://codeberg.org/librewolf/source/releases/tag" | jq -r '.[0] | .name')
 
 
 

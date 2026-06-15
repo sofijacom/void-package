beee68aaa4d37fdca49dfa498df866a80ea001fe
sofijacom <sofija.p2018@gmail.com>
2026-06-15 06:28:49 +0300

    Update

--- srcpkgs/librewolf/update.sh
+++ srcpkgs/librewolf/update.sh
@@ -10,9 +10,7 @@ echo "### Checking for librewolf updates..."
 
 # LATEST_VERSION=$(curl -sL https://api.codeberg.org/repos/ваш_пользователь/ваш_репозиторий/releases/latest | jq -r ".tag_name")
 
-# LATEST_VERSION=$(curl -sL https://api.codeberg.org/repos/${REPO}/releases/latest | jq -r '.tag_name')
-
-LATEST_VERSION=$(curl -sL https://api.codeberg.org/repos/${REPO}/releases/latest | grep "tag_name")
+LATEST_VERSION=$(curl -s "https://api.codeberg.org/repos/librewolf/source/releases" | jq -r '.tag_name')
 
 VERSION=${LATEST_VERSION#"v"}
 CUR_VERSION=$(grep -E '^version=' ${__dir}/template | cut -d= -f2)

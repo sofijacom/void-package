a21929e8e05cd2b7d20770677db48f4d8f34d6d5
Sofiya <107557749+sofijacom@users.noreply.github.com>
2026-06-21 02:14:59 +0300

    Update update.sh
    Signed-off-by: Sofiya <107557749+sofijacom@users.noreply.github.com>

--- srcpkgs/librewolf/update.sh
+++ srcpkgs/librewolf/update.sh
@@ -10,7 +10,7 @@ __dir="$(dirname "${BASH_SOURCE[0]}")"
 
 echo "### Checking for librewolf updates..."
 
-# LATEST_VERSION=$(curl -sL "https://codeberg.org/api/v1/repos/your user/your repository/releases/${ID}" | jq -r ".tag_name")
+# LATEST_VERSION=$(curl -sL "https://codeberg.org/api/v1/repos/your user/your repository/releases/latest" | jq -r ".tag_name")
 LATEST_VERSION=$(curl -s "https://codeberg.org/api/v1/repos/librewolf/source/releases/${ID}" | jq -r ".tag_name")
 
 VERSION=${LATEST_VERSION#"v"}

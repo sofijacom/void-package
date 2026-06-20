14fe3ed1a1b6d39fd8ba896da8b0658c8243dc71
Sofiya <107557749+sofijacom@users.noreply.github.com>
2026-06-20 11:59:27 +0300

    Add files via upload
    Signed-off-by: Sofiya <107557749+sofijacom@users.noreply.github.com>

---
+++ srcpkgs/ly/update.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+printf "Checking latest version\n"
+
+__dir="$(dirname "${BASH_SOURCE[0]}")"
+
+REPO="fairyglade/ly"
+TEMPLATE=${__dir}/template
+ID="9341631"
+
+LATEST_VERSION=$(curl -s "https://codeberg.org/api/v1/repos/${REPO}/releases/${ID}" | jq -r ".tag_name")
+export VERSION=${LATEST_VERSION#"v"}
+CURRENT_VERSION=$(grep -E '^version=' "${TEMPLATE}" | cut -d= -f2)
+
+printf "Latest version is: %s\nLatest built version is: %s\n" "${VERSION}" "${CURRENT_VERSION}"
+[ "${CURRENT_VERSION}" = "${VERSION}" ] && printf "No new version to release\n" && exit 0
+
+# No preprepped checksum files, need to download the binary and calculate it myself
+gh release download -R ${REPO} --archive=tar.gz --output "ly.tar.gz"
+export SHA256=$(sha256sum ./ly.tar.gz | cut -d ' ' -f1 )
+rm ./ly.tar.gz
+[[ -n ${SHA256} && ${SHA256} =~ ^[A-Fa-f0-9]{64}$ ]] && printf "got junk instead of sha256\n" && exit 1
+
+sed -i "s/^version=.*/version=$VERSION/" "$TEMPLATE"
+sed -i "s/^checksum=.*/checksum=$SHA256/" "$TEMPLATE"
+
+printf "ly template updated\n"

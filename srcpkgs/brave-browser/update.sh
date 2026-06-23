14bfa72935d0ec2de4a959fa13b54792d2ef40b8
sofijacom <sofija.p2018@gmail.com>
2026-06-23 23:35:55 +0300

    Update

--- srcpkgs/brave-browser/update.sh
+++ srcpkgs/brave-browser/update.sh
@@ -16,7 +16,7 @@ printf "Latest version is: %s\nLatest built version is: %s\n" "${VERSION}" "${CU
 [ "${CURRENT_VERSION}" = "${VERSION}" ] && printf "No new version to release\n" && exit 0
 
 export SHA256=$(curl -L https://github.com/${REPO}/releases/download/v${VERSION}/brave-browser-${VERSION}-linux-amd64.zip.sha256 | cut -d ' ' -f1 )
-[[ -n ${SHA256} && ${SHA256} =~ ^[A-Fa-f0-9]{64}$ ]] && printf "got junk instead of sha256\n" && exit 1
+[[ ! ${SHA256} =~ ^[a-z0-9]+$ ]] && printf "got junk instead of sha256\n" && exit 1
 
 sed -i "s|^version=.*$|version=${VERSION}|" "${TEMPLATE}"
 sed -i "s|^checksum=.*$|checksum=${SHA256}|" "${TEMPLATE}"

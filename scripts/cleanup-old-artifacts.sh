64414f439531c8c0409f5c7ac075ff388de080de
sofijacom <107557749+sofijacom@users.noreply.github.com>
2026-07-02 16:42:18 +0300

    Update cleanup-old-artifacts.sh

--- scripts/cleanup-old-artifacts.sh
+++ scripts/cleanup-old-artifacts.sh
@@ -2,10 +2,10 @@
 
 # gh auth login
 # Замените <owner/repo> на ваш репозиторий (например, my-org/my-repo)
-REPO=$1  # sofijacom/void-package
+REPO=sofijacom/void-package
 DAYS_OLD=5  # Удаляем артефакты старше 5 дней
 
-echo "Cleaning up artifacts older than $DAYS_OLD days for repository: $REPO"
+echo "Cleaning up artifacts older than $DAYS_OLD days for repository:$REPO"
 
 # Функция для удаления артефакта по ID
 delete_artifact() {

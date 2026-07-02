3db7466f242d282403e6510d1d7f111c86be31f1
sofijacom <107557749+sofijacom@users.noreply.github.com>
2026-07-03 01:50:44 +0300

    Update cleanup-old-artifacts.sh

--- scripts/cleanup-old-artifacts.sh
+++ scripts/cleanup-old-artifacts.sh
@@ -46,8 +46,8 @@ while true; do
 
     # Calculate artifact age in days (macOS-compatible)
     # CREATED_AT_SECONDS=$(date -j -f "%Y-%m-%dT%H:%M:%SZ" "$CREATED_AT" +%s)
-    # CREATED_AT_SECONDS=$(date -f "%Y-%m-%dT%H:%M:%SZ" +"$CREATED_AT")
-    CREATED_AT_SECONDS=$(date -j -f "%Y-%m-%dT%H:%M:%SZ" "$CREATED_AT" "+%s")
+    # CREATED_AT_SECONDS=$(date -j -f "%Y-%m-%dT%H:%M:%SZ" "$CREATED_AT" "+%s")
+    CREATED_AT_SECONDS=$(date -f "%Y-%m-%dT%H:%M:%SZ" "+$CREATED_AT")
     CURRENT_TIME=$(date +%s)
     AGE_DAYS=$(( (CURRENT_TIME - CREATED_AT_SECONDS) / 86400 ))
 
